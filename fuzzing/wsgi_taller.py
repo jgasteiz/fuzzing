64bd80de8e8066c5965eb36159b063c5fb95f2a3
Javi Manzano <javi.manzano.oller@gmail.com>
2015-03-22 22:53:25 +0000

    New wsgi per app

--- fuzzing/wsgi_taller.py
+++ fuzzing/wsgi_taller.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'fuzzing.settings.common'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'fuzzing.settings.taller'
 application = get_wsgi_application()
